79549c39c069dc34a8bfc426734450aa880efe95
Naoji <nt.0118@gmail.com>
2024-02-01 09:51:51 +0900

    Change config name

--- src/server.go
+++ src/server.go
@@ -1,17 +1,35 @@
+// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	srv := http.Server{
-		Addr: ":80",
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
 
-	http.HandleFunc("/", helloWorld)
-
-	srv.ListenAndServe()
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!\n"))
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
