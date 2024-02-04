6c8ac5fadc309765072067cc1f0a5fd22973794d
Naoji <nt.0118@gmail.com>
2024-02-04 19:59:31 +0900

    add graceful shutdown

--- src/main.go
+++ src/main.go
@@ -2,7 +2,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Naoji1984/go-rana8/config"
 	"github.com/Naoji1984/go-rana8/listener"
@@ -14,5 +19,20 @@ func main() {
 	e := echo.New()
 	listener.Init(e)
 
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", config.Config.AppPort)))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	// Start server
+	go func() {
+		if err := e.Start(fmt.Sprintf(":%s", config.Config.AppPort)); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
